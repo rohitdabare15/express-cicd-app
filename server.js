01e44275c0877d92fad0262ebd358171361fb9cd
RohitDabare <dabarerohit@gmail.com>
2026-02-28 00:10:26 +0530

    Updates

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Express CI/CD App Running Successfully!');
+    res.send('Express CI/CD Deployed Successfully!');
 });
 
 app.listen(3000, '0.0.0.0', () => {
-    console.log('Server started on port 3000');
+    console.log('Server running on port 3000');
 });
